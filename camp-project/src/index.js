0793d18eba235974e50de3169fec9fa19db5c751
Ceyda Kamalı <ceydakamali3@gmail.com>
2021-06-06 01:23:52 +0300

    lecture 11

---
+++ camp-project/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(//rendera 2 parametre gönderiyoruz.
+  
+    <App/>,document.getElementById('root')
+
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+//Dom:program çalışırken arka tarafta oluşan html yapısına denir.
